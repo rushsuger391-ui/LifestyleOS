4b8d9797f1a4efe9c31877f2229972cf8ecc4bbe
rushsuger391-ui <rushsuger391@gmail.com>
2025-12-02 00:13:51 +0500

    Update memory.py

--- backend/memory.py
+++ backend/memory.py
@@ -1,1 +1,10 @@
+class MemoryModule:
+    def __init__(self):
+        self.storage = {}
+
+    def remember(self, key, value):
+        self.storage[key] = value
+
+    def recall(self, key):
+        return self.storage.get(key)
 
